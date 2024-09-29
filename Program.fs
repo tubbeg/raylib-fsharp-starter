3fc1ff255da25b335ca58d4fa47fd5d5ebd7e515
Tobias Gärdebro <tubbeg@gmail.com>
2024-09-29 20:09:08 +0200

    First commit

---
+++ Program.fs
@@ -0,0 +1,29 @@
+﻿open Raylib_cs
+
+printfn "Running Raylib"
+
+let cboolToBool (c : CBool) : bool =
+    CBool.op_Implicit c
+
+let windowRayLib ()  =
+    let rec wraylib isClose  =
+        match isClose with
+        | true -> ()
+        | _ ->
+            Raylib.BeginDrawing()
+            Raylib.ClearBackground(Color.White)
+            Raylib.DrawText("Hello, world!", 12, 12, 20, Color.Black)
+            Raylib.EndDrawing()
+            Raylib.WindowShouldClose() |> cboolToBool |> wraylib
+    wraylib false
+
+
+let runRayLib () =
+    Raylib.InitWindow(800, 480, "Hello World")
+    Raylib.BeginDrawing()
+    windowRayLib()
+    Raylib.EndDrawing()
+
+
+runRayLib ()
+printfn "Done!"
